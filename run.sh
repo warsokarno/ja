676cb04e92142ca0575b37866655279632a30ab2
wsrk <165033583+warsokarno@users.noreply.github.com>
2024-05-28 18:45:13 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-da08331e-ea5c-4cd9-b917-4ab5c3bbc5e4/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-133a67ce-c525-4b51-816b-c3d752d07dbf/)
 
 
 echo "$curl_output" >> Update.tsx
